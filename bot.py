7955971d5eed22fd83ee46163f500b066149259f
Botir Mahmudov <botir6@gmail.com>
2026-02-19 21:59:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1024,40 +1024,30 @@ async def show_masters(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # 👑 VIP BLOK
 # ==========================
     if page == 0:
-        for mid, name, phone, dist, age, experience in vip_rows:
-
-            c.execute("SELECT AVG(rating), COUNT(*) FROM ratings WHERE master_id=%s", (mid,))
-            avg_rating, votes = c.fetchone()
-
-            rating_text = (
-                f"⭐ {round(avg_rating,1)} ({votes})"
-                if avg_rating else "⭐ Рейтинг йўқ"
-            )
-            line = "═══════════════════════════"
+        for mid, name, phone, dist, age, experience in normal_rows:
 
             card = f"""
-            ━━━━━━━━━━━━━━
-            <b>👑 VIP УСТА</b>
-            ━━━━━━━━━━━━━━
+        <b>👷 УСТА</b>
 
-            👤 <b>{name}</b>
-            📍 Қарши ш.
-            🎂 {age} ёш
-            🧰 {experience} йил
-            📞 <b>+{phone}</b>
-            ⭐ {rating_text}
-            """
+        👤 <b>{name}</b>
+        📍 {dist}\n"
+        🎂 {age} ёш
+        🧰 {experience} йил
+        📞 <b>+{phone}</b>
+        ⭐ {rating_text}
+        """
 
+            keyboard = [[
+        InlineKeyboardButton("📞 Қўнғироқ", callback_data=f"call_{phone}"),
+        InlineKeyboardButton("✅ Чақирдим", callback_data=f"order_{mid}"),
+        InlineKeyboardButton("⭐ Баҳо", callback_data=f"rate_{mid}")
+            ]]
 
-            keyboard = [
-                [InlineKeyboardButton("📞 Қўнғироқ", callback_data=f"call_{phone}"),
-                InlineKeyboardButton("✅ Чақирдим", callback_data=f"order_{mid}"),
-                InlineKeyboardButton("⭐ Баҳо", callback_data=f"rate_{mid}")]
-            ]
-
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            
-            await message.reply_text(card, parse_mode="HTML", reply_markup=reply_markup)
+            await message.reply_text(
+                card,
+                parse_mode="HTML",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+            )
  
     # ==========================
     # 📋 NORMAL BLOK
@@ -1569,6 +1559,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
